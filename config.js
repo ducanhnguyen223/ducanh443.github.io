7f848afd9a9f6b06ff448924f26b7fd500093a73
ducanhnguyen223 <anhndgch220882@fpt.edu.vn>
2025-03-03 00:11:50 +0700

    birthday

---
+++ config.js
@@ -0,0 +1,26 @@
+
+var config = {
+    texts: [
+        "Chúc mừng sinh nhật",  
+        "Em Bé Thỏ",  
+        "Hãy biến ngày hôm nay trở thành một ngày đặc biệt đầy niềm vui, hạnh phúc và thành công trong cuộc sống.",
+        "Mong rằng tuổi mới này sẽ đem đến nhiều niềm vui, sức khỏe tốt và đủ sự thành công để làm tỏa sáng cuộc sống của mình.",
+        "Hãy trân trọng những khả năng và thành tựu của mình, và luôn luôn tin tưởng vào bản thân để đạt được những mục tiêu lớn trong cuộc sống.",
+        "Và hy vọng em sẽ có một ngày đầy niềm vui, tiếng cười và những kỷ niệm đáng nhớ cùng những người thân yêu của mình.",
+    ],
+    imgs: {
+        "Em Bé Thỏ": "./imgs/cmsn.png",
+        
+    },
+    desc: {
+        turn_on: "Bật đèn",
+        play: "Phát nhạc",
+        bannar_coming: "Trang trí thêm nào.",
+        balloons_flying: "Thả bóng bay",
+        cake_fadein: "Bánh sinh nhật",
+        light_candle: "Hãy thắp nến nào.",
+        wish_message: "Hãy ước đi nào.",
+        story: "Vài lời chúc dành cho Em.",
+        cake_cut: "Còn 1 điều bất ngờ",
+    }
+};
